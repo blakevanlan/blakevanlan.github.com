8e24a720d1c8879b02b8f90c18f0f7a413799ae7
Blake VanLandingham <blake.vanlandingham@softekinc.com>
2012-08-09 13:24:34 -0500

    Added iOS fix for orientation change. Styled h1 and sidebar.

--- js/script.js
+++ js/script.js
@@ -2,4 +2,15 @@
 $(document).ready(function(){
 
 });
-})(jQuery)
+})(jQuery);
+
+/* iOS orientation change bug fix */
+if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
+  var viewportmeta = document.querySelector('meta[name="viewport"]');
+  if (viewportmeta) {
+    viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0';
+    document.body.addEventListener('gesturestart', function() {
+      viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
+    }, false);
+  }
+}
